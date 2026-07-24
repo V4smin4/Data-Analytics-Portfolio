e518970a38773fe5bcf9f9a71e2bae93eb21f323
V4smin4 <yasmina.01027285708@gmail.com>
2026-07-25 00:40:16 +0300

    Update queries.sql

--- Titanic-SQL-Analysis/queries.sql
+++ Titanic-SQL-Analysis/queries.sql
@@ -1,37 +1,37 @@
 -- Total passengers
 SELECT COUNT(*) AS Total_Passengers
-FROM titanic;
+FROM train;
 
 -- Overall survival rate
 SELECT ROUND(AVG(Survived) * 100, 2) AS Survival_Rate
-FROM titanic;
+FROM train;
 
 -- Survival by gender
 SELECT
     Sex,
     ROUND(AVG(Survived) * 100, 2) AS Survival_Rate
-FROM titanic
+FROM train
 GROUP BY Sex;
 
 -- Survival by passenger class
 SELECT
     Pclass,
     ROUND(AVG(Survived) * 100, 2) AS Survival_Rate
-FROM titanic
+FROM train
 GROUP BY Pclass;
 
 -- Average age by passenger class
 SELECT
     Pclass,
     ROUND(AVG(Age), 2) AS Average_Age
-FROM titanic
+FROM train
 GROUP BY Pclass;
 
--- Bonus: Survival rate by gender AND class together
+-- Bonus: Survival rate by gender AND class
 SELECT
     Pclass,
     Sex,
     ROUND(AVG(Survived) * 100, 2) AS Survival_Rate
-FROM titanic
+FROM train
 GROUP BY Pclass, Sex
 ORDER BY Pclass, Sex;
